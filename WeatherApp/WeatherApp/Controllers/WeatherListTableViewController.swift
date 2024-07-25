06eefa50a907db64fba89580c32fd53733d9662b
Jason <sanchezjason@me.com>
2024-07-25 17:36:14 -0600

    good_weather/uikit/weather_app- NavBarItem.appearance()

--- WeatherApp/WeatherApp/Controllers/WeatherListTableViewController.swift
+++ WeatherApp/WeatherApp/Controllers/WeatherListTableViewController.swift
@@ -16,6 +16,21 @@ class WeatherListTableViewController: UITableViewController {
 
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        
+        cell.textLabel?.text = "Boulder"
+        
+        return cell
+    }
     
 }
