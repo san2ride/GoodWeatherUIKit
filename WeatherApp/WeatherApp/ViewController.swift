77459b2a8e6fb4070d233f6047cf59a2db5f6efc
Jason <sanchezjason@me.com>
2024-07-25 15:49:23 -0600

    good_weather/uikit/weather_app- basic storyboard structure setup

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jason Sanchez on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
