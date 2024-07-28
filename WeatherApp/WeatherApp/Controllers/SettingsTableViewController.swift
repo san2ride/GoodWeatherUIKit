090eca8ab31744a669ad8860dd7b95e9e79e6f24
Jason <sanchezjason@me.com>
2024-07-28 17:34:00 -0600

    anf_code_test/explore- Settings

--- WeatherApp/WeatherApp/Controllers/SettingsTableViewController.swift
+++ WeatherApp/WeatherApp/Controllers/SettingsTableViewController.swift
@@ -8,9 +8,33 @@
 import UIKit
 
 class SettingsTableViewController: UITableViewController {
+    
+    private var settingsViewModel = SettingsViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+    
+    func setup() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
 
+extension SettingsTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingsViewModel.units.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let settingsItem = settingsViewModel.units[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingsCell", for: indexPath)
+        cell.textLabel?.text = settingsItem.displayName
+        return cell
     }
 }
