fff1ebb0b422b1d4f24d2a5f79e80ff4a67c300e
Jason <sanchezjason@me.com>
2024-07-28 17:17:28 -0600

    anf_code_test/explore- SettingsViewModel

---
+++ WeatherApp/WeatherApp/Controllers/SettingsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsTableViewController.swift
+//  WeatherApp
+//
+//  Created by Jason Sanchez on 7/28/24.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ WeatherApp/WeatherApp/ViewModels/SettingsViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SettingsViewModel.swift
+//  WeatherApp
+//
+//  Created by Jason Sanchez on 7/28/24.
+//
+
+import Foundation
+
+enum Unit: String, CaseIterable {
+    case celsius = "metric"
+    case fahrenheit = "imperial"
+}
+
+extension Unit {
+    var displayName: String {
+        get {
+            switch(self) {
+                case .celsius:
+                    return "Celsius"
+                case .fahrenheit:
+                    return "Fahrenheit"
+            }
+        }
+    }
+}
+
+class SettingsViewModel {
+    let units = Unit.allCases
+    
+    var selectedUnit: Unit {
+        get {
+            let userDefaults = UserDefaults.standard
+            var unitValue = ""
+            if let value = userDefaults.value(forKey: "unit") as? String {
+                unitValue = value
+            }
+            return Unit(rawValue: unitValue)!
+        }
+        set {
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(newValue.rawValue, forKey: "unit")
+        }
+    }
+    
+}
